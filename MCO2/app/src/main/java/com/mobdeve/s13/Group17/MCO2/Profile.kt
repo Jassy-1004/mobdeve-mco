a0b9f9b1471c6cda7cd160a3fda97c31e34a7f4b
Jasmin Chua <jasmin_chua@dlsu.edu.ph>
2023-03-12 14:07:47 +0800

    added in profile.kt

--- MCO2/app/src/main/java/com/mobdeve/s13/Group17/MCO2/Profile.kt
+++ MCO2/app/src/main/java/com/mobdeve/s13/Group17/MCO2/Profile.kt
@@ -1,4 +1,12 @@
 package com.mobdeve.s13.Group17.MCO2
 
-class Profile {
-}
+class Profile (
+    val username: String,
+    val firstName: String,
+    val lastName: String,
+    val email: String,
+    val password: String,
+    val gender: String,
+    val birthday: String,
+    var bio: String
+)
